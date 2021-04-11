2bef889ac54ccd2a60b79d84e5a791c808790c7d
Ricardo De la Maza <radelamaza@uc.cl>
2021-04-10 20:10:45 -0400

    Updates

---
+++ precache-manifest.a02ec22a628c092513149fc887c600ed.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e2762a0c17a31d60ce2f61c69c1f249",
+    "url": "/RDLMovies/index.html"
+  },
+  {
+    "revision": "0f0bd9ffb16a17396c62",
+    "url": "/RDLMovies/static/css/3.8beb1efa.chunk.css"
+  },
+  {
+    "revision": "c13f65af3801686781f1",
+    "url": "/RDLMovies/static/css/main.8cfbc9c3.chunk.css"
+  },
+  {
+    "revision": "7163ecfcc7ea3529d200",
+    "url": "/RDLMovies/static/js/0.06abb9ed.chunk.js"
+  },
+  {
+    "revision": "b4eb4d7040b35cde4033",
+    "url": "/RDLMovies/static/js/10.b1340f7b.chunk.js"
+  },
+  {
+    "revision": "4e0a79ca00e3216a493f",
+    "url": "/RDLMovies/static/js/11.575809c4.chunk.js"
+  },
+  {
+    "revision": "0f0bd9ffb16a17396c62",
+    "url": "/RDLMovies/static/js/3.7126be44.chunk.js"
+  },
+  {
+    "revision": "0ddd194f5bd322226304e8e5072588e2",
+    "url": "/RDLMovies/static/js/3.7126be44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6a09b10bc26158bbd16",
+    "url": "/RDLMovies/static/js/4.33521e0d.chunk.js"
+  },
+  {
+    "revision": "ad9e01b92d5419ee013f",
+    "url": "/RDLMovies/static/js/5.2717c346.chunk.js"
+  },
+  {
+    "revision": "1e54968d912de8b18397",
+    "url": "/RDLMovies/static/js/6.ac122c5d.chunk.js"
+  },
+  {
+    "revision": "b846f2814db424fe1433",
+    "url": "/RDLMovies/static/js/7.32dc3594.chunk.js"
+  },
+  {
+    "revision": "1e557b3255c0c92635a1",
+    "url": "/RDLMovies/static/js/8.60c40040.chunk.js"
+  },
+  {
+    "revision": "d861b36b0f7eb379ee45",
+    "url": "/RDLMovies/static/js/9.ce50031f.chunk.js"
+  },
+  {
+    "revision": "c13f65af3801686781f1",
+    "url": "/RDLMovies/static/js/main.5a3cf64b.chunk.js"
+  },
+  {
+    "revision": "86fc685664c82fcbb696",
+    "url": "/RDLMovies/static/js/runtime-main.86bf125e.js"
+  }
+]);
